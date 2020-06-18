b5899a810c8bf6cda2afe7fb4fa1cf58e9c2c055
weichenz <weichenz@I-M404J1GG-1158.local>
2020-06-18 09:59:02 +0800

    feat: header样式

--- jianshu/src/App.js
+++ jianshu/src/App.js
@@ -1,15 +1,13 @@
 import React, { Fragment } from 'react';
+import Header from './common/header'
 import { GlobalStyle } from './style'
 
 function App() {
   return (
     <Fragment>
       <GlobalStyle />
-      <div>
-       hello world
-      </div>
+      <Header />
     </Fragment>
-    
   );
 }
 
